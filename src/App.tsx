84fb82c7045a41e2ad3ddd09ee6de9cf92a25ea1
elcasey <temetvince@gmail.com>
2025-11-04 23:17:45 -0600

    Simplify CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
 import Particles from './particles';
 
-const App: React.FC = () => {
+const App = () => {
+  useEffect(() => {
+    // Add global styles to ensure proper layout
+    const style = document.createElement('style');
+    style.innerHTML = `
+         body, html {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            width: 100%;
+            height: 100%;
+         }
+      `;
+    document.head.appendChild(style);
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, []);
+
   return (
     <div className='app'>
       <Particles />
